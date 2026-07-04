ef037d98f98f32f7d8c8cb8809739cc32f0f9e5b
cosand64 <cosand64@gmail.com>
2026-07-03 18:17:08 -0600

    finished this week's nps

--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -219,8 +219,7 @@ async function getJson(url) {
 }
 
 export function getInfoLinks(data) {
-  // Why index + 2 below? no real reason. we don't want index 0 since that is the one we used for the banner...I decided to skip an image.
-  const withUpdatedImages = parkInfoLinks.map((item, index) => {
+    const withUpdatedImages = parkInfoLinks.map((item, index) => {
     item.image = data[index + 2].url;
     return item;
   });
@@ -228,6 +227,21 @@ export function getInfoLinks(data) {
 }
 
 export async function getParkData() {
-  const parkData = await getJson("parks?parkCode=yell");
+  const parkData = await getJson("parks?parkCode=yell ");
+  return parkData.data[0];
+}
+
+export async function getParkAlerts(code) {
+  const parkData = await getJson(`alerts?parkCode=${code}`);
+  return parkData.data;
+}
+
+export async function getParkVisitorCenters(code) {
+  const parkData = await getJson(`visitorcenters?parkCode=${code}`);
+  return parkData.data;
+}
+
+export async function getParkVisitorCenterDetails(id) {
+  const parkData = await getJson(`visitorcenters?id=${id}`);
   return parkData.data[0];
 }
